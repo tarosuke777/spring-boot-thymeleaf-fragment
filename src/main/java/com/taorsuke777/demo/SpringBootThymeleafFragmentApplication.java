dd33938d3b337816528fcd24f5d3e4e98f7cbfb0
tarosuke <felis.silvestris.catus.7777@gmail.com>
2025-03-29 12:42:59 +0900

    first commit

---
+++ src/main/java/com/taorsuke777/demo/SpringBootThymeleafFragmentApplication.java
@@ -0,0 +1,13 @@
+package com.taorsuke777.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafFragmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafFragmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taorsuke777/demo/SpringBootThymeleafFragmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taorsuke777.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeleafFragmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
